fafebbc0a2d7f850bf160301524db265fc9682b8
lhadiyousfi1@gmail.com <lhadiyousfi1@gmail.com>
2024-04-08 23:36:59 +0200

    Implementation of a constructors

--- Dataframe/src/main/java/Dataframe/col.java
+++ Dataframe/src/main/java/Dataframe/col.java
@@ -1,10 +1,23 @@
 
 package Dataframe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class col <VALUE extends Object> {
   private String label;
     private String type;
     private List<VALUE> values;
+    
+
+
+public col(String label, String type, List<VALUE> values){
+    this.label = label;
+    this.type = type;
+    this.values = values;
 }
+public col(String label, String type){
+    this.label = label;
+    this.type = type;
+    this.values = new ArrayList<VALUE>();
+}}
