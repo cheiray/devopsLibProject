e079147a1d72fcb2fd7764d1586eef247c9c2dac
cheiray <rayencheikh@gmail.com>
2024-04-17 23:07:47 +0200

    Code skeleton

---
+++ Dataframe/src/main/java/Dataframe/Dataframe.java
@@ -0,0 +1,110 @@
+package Dataframe;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rayen CHEIKH M'HAMED, L'HADI YOUSFI, Benjamin
+ */
+
+public class Dataframe {
+ 
+    private ArrayList<String> labels;
+    private List<Column> data;
+    private int numberOfLines;
+
+    // Empty Default constructor
+    public Dataframe() {
+        
+        labels=new ArrayList<String>();
+        data =new ArrayList<Column>();
+        numberOfLines=0;
+
+    }
+
+    
+    public Dataframe( Map<String, List<?>> map) throws BadArgumentException{
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+    public Dataframe(String nomFichier) throws FileNotFoundException, IOException{
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+
+    public int getDataFrameSize() {
+        return numberOfLines;
+    }
+
+
+    protected void displayFromTo(int debut, int fin) throws BadArgumentException{
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+        
+    }
+
+    public void displayFromBegginingTo(int nbline) throws BadArgumentException {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+    public void displayFromEndTo(int nbline) throws BadArgumentException {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+
+    public Dataframe selectLine(int debut ,int fin ){
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+    public Dataframe selectCol(ArrayList<String> l){
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+
+    public List<String> getligne(int index) {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+
+    }
+
+
+    public Column getcol(String label)  {
+
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+    public void insertligne(String[] l) {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");       
+    }
+
+    public void insertCol(Column c) {
+       System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+
+    public double max(String label) throws BadArgumentException {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+    
+
+    public double sum(String label) throws BadArgumentException {
+      System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+ 
+    public double min(String label) throws BadArgumentException {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+    
+    
+    public double mean(String label) throws BadArgumentException {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+    public void deleteRow(int index) {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+    public void deleteColumn(String label) {
+        System.out.println("TO IMPLEMENT: This method needs to be implemented");
+    }
+
+}
+   
